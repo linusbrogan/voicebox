c40be250960995cddb42a6c83ae74b3cfd8b99a6
Linus Brogan <github@linusaur.us>
2019-05-20 16:44:59 -0700

    build v0.1.0

---
+++ precache-manifest.85a017394cc2425b95c349f7c37bbe9b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dee5b242cec3efda81287f56efc918de",
+    "url": "/voicebox/index.html"
+  },
+  {
+    "revision": "35d89c1f5c52849c813b",
+    "url": "/voicebox/static/css/main.fd5fc7c1.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/voicebox/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "35d89c1f5c52849c813b",
+    "url": "/voicebox/static/js/main.53c49775.chunk.js"
+  },
+  {
+    "revision": "3cafe6d9cc3d65b1ed93",
+    "url": "/voicebox/static/js/runtime~main.260ed36c.js"
+  }
+]);
